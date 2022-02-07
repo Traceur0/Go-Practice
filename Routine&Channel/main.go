e8165900e91bfd79f80a02ba1191157fe4efe1c2
Traceur0 <ghost01ghost@gmail.com>
2022-02-07 14:32:18 +0900

    'nameHere' is Awesome!

--- Routine&Channel/main.go
+++ Routine&Channel/main.go
@@ -1,7 +1,24 @@
 package main
 
-import
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	return
+	channel_1 := make(chan string) // gochannel
+	people := [2]string{"git", "github"}
+	for _, person := range people {
+		go isAwesome(person, channel_1) // goroutine
+	}
+	fmt.Println("Waiting for channel data")
+	fmt.Println("Received data: ", <-channel_1)
+	fmt.Println("Received data: ", <-channel_1)
+}
+
+
+func isAwesome(person string, channel_1 chan string) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	channel_1 <- person + " is Awesome!"
 }
