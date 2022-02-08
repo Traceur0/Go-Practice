6c51d889722752030074c3ab91bd465636ee3591
Traceur0 <ghost01ghost@gmail.com>
2022-02-08 10:12:21 +0900

    confirming this files: Routine&Channel

--- Routine&Channel/main.go
+++ Routine&Channel/main.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
 	channel_1 := make(chan string) // gochannel
-	people := [2]string{"git", "github"}
+	people := [4]string{"git", "github", "Visual Studio Code", "atom"}
 	for _, person := range people {
 		go isAwesome(person, channel_1) // goroutine
 	}
-	fmt.Println("Waiting for channel data")
-	fmt.Println("Received data: ", <-channel_1)
-	fmt.Println("Received data: ", <-channel_1)
+	for i := 0; i < len(people); i++ {
+		fmt.Print("Waiting for ", i)
+		fmt.Println(<-channel_1) // blocking operation : receiving channel messages
+	}
 }
 
 
 func isAwesome(person string, channel_1 chan string) {
 	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	channel_1 <- person + " is Awesome!"
+	channel_1 <- person + " is Awesome!" // sending channel message(info: int, string, boolean, whatever..)
 }
